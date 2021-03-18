1acc87b97c4364158c9b0c2d99f644aed2384112
Aaron <candyman633@gmail.com>
2021-03-17 20:26:02 -0500

    flexbox working. Header and footer placeholder working. Recoilize debugger working

--- keybinds-unbound/src/App.js
+++ keybinds-unbound/src/App.js
@@ -1,30 +1,36 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Row, Col } from 'reactstrap';
 import {
   atom,
-  selector,
-  useRecoilState,
+  // selector,
+  // useRecoilState,
   useRecoilValue,
 } from 'recoil';
 
+const aThing = atom({
+  key: "aThing",
+  default: "Something COOOL"
+})
 
 function App() {
-
-  const testAtom = atom({
-    key: "something",
-    default: "value"
-  })
-
-  const aThing = useRecoilValue(testAtom)
+  const garble = useRecoilValue(aThing)
     
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          stuff and things {aThing}
+          stuff and things: {garble}
         </p>
       </header>
+      <Col className="Body">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Body</p>
+      </Col>
+      <footer>
+        <p>Footer</p>
+      </footer>
     </div>
   );
 }
